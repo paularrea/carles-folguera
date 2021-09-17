d914e5833bf36b89b4e125af4acf97a38cff1eae
Pau Larrea <paularrea12@hotmail.com>
2021-09-17 11:46:24 +0200

    cursor

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,22 +1,24 @@
-import React from "react";
-import PropTypes from "prop-types";
-import Header from "../header/Header";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react"
+import PropTypes from "prop-types"
+import Header from "../header/Header"
+import "bootstrap/dist/css/bootstrap.min.css"
 import "../../styles/App.scss"
-import {page} from "./layout.module.scss"
+import { page } from "./layout.module.scss"
+import AnimatedCursor from "../cursor/AnimatedCursor"
 
 const Layout = ({ children }) => {
   if (typeof window !== "undefined") {
-    require("smooth-scroll")('a[href*="#"]');
+    require("smooth-scroll")('a[href*="#"]')
   }
   return (
     <>
+      <AnimatedCursor />
       <Header />
       <main className={page}>{children}</main>
     </>
-  );
-};
+  )
+}
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
-};
-export default Layout;
+}
+export default Layout
--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -3,9 +3,11 @@ import Seo from "../../components/seo"
 import CarouselContainer from "../../components/carousel/CarouselContainer"
 import ProjectsGrid from "../../components/grid/ProjectsGrid"
 import ProjectsHeader from "../../components/header/ProjectsHeader"
+import AnimatedCursor from "../../components/cursor/AnimatedCursor"
 
 const Projects = () => (
   <>
+    <AnimatedCursor />
     <ProjectsHeader />
     <Seo title="Projects" />
     <CarouselContainer />
