f2cb67cad55f6913ed3416755c551591b6f9122b
Pau Larrea <paularrea12@hotmail.com>
2022-01-17 15:43:01 +0100

    switcher style done

--- src/components/darkMode/DarkModeToggle.jsx
+++ src/components/darkMode/DarkModeToggle.jsx
@@ -1,20 +1,24 @@
 import React from "react"
 import { ThemeToggler } from "gatsby-plugin-dark-mode"
 import { Switch } from "./Switch"
+import {container} from "./switcher.module.scss"
 
 const DarkModeToggle = () => {
   return (
-    <ThemeToggler>
-      {({ theme, toggleTheme }) => {
-        return (
-          <Switch
-            type="checkbox"
-            onChange={e => toggleTheme(e.target.checked ? "dark" : "light")}
-            checked={theme === "dark"}
-          />
-        )
-      }}
-    </ThemeToggler>
+    <div className={container}>
+      <p>Dark mode</p>
+      <ThemeToggler>
+        {({ theme, toggleTheme }) => {
+          return (
+            <Switch
+              type="checkbox"
+              onChange={e => toggleTheme(e.target.checked ? "dark" : "light")}
+              checked={theme === "dark"}
+            />
+          )
+        }}
+      </ThemeToggler>
+    </div>
   )
 }
 
